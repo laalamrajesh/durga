056763f289071b7d72b19d87cdc28a8351896b2f
durga1505 <52831966+durga1505@users.noreply.github.com>
2019-08-14 12:59:04 +0530

    Update Account.java

--- programs/Account.java
+++ programs/Account.java
@@ -19,58 +19,75 @@ class Account
 		
 	}
 }
+class IsValid
+{
+	int i;
+	public int validName(String name,int accept)
+	{
+		for(i=0;i<name.length();i++)
+		{
+			if((int)name.charAt(i)>=65&&(int)name.charAt(i)<=90 || (int)name.charAt(i)>=97&&(int)name.charAt(i)<=122)
+			{
+				accept=1;
+			}
+			else
+			{
+				accept = 0;
+				System.out.println("Invalid Name Entered");
+				break;
+			}
+		}
+		return accept;
+	}
+	public int validAccountNumber(String accountNumber,int accept)
+	{
+		
+		for(i=0;i<accountNumber.length();i++)
+		{
+			if(accountNumber.charAt(i)>=48 && accountNumber.charAt(i)<=57)
+				accept=1;
+			else
+			{
+				accept = 0;
+				System.out.println("Invalid Acc No ** Must contain numbers");
+				break;
+			}
+		}
+		if(accept==1)
+		{
+			if(accountNumber.length()!=11)
+			{
+				System.out.println("Invalid Acc No ** Length must be 11");
+				accept=0;
+			}
+		}
+		return accept;
+	}
+}
 class Demo
 {
 	public static void main(String args[])
 	{
 		Scanner sc = new Scanner(System.in);
-		int i,accept;
+		int accept;
 		String name,accountNumber;
+		IsValid iv = new IsValid();
 		do
 		{
 			accept=0;
 			System.out.println("Accountant Name: ");
 			name = sc.next();
-			for(i=0;i<name.length();i++)
-			{
-				if((int)name.charAt(i)>=65&&(int)name.charAt(i)<=90 || (int)name.charAt(i)>=97&&(int)name.charAt(i)<=122)
-				{
-					accept=1;
-				}
-				else
-				{
-					accept = 0;
-					System.out.println("Invalid Name Entered");
-					break;
-				}
-			}
+			accept = iv.validName(name,accept);
 		}while(accept==0);
-		
 		do
 		{
 			accept=0;
 			System.out.println("Account Number: ");
 			accountNumber = sc.next();
-			for(i=0;i<accountNumber.length();i++)
-			{
-				if(accountNumber.charAt(i)>=48 && accountNumber.charAt(i)<=57)
-					accept=1;
-				else
-				{
-					accept = 0;
-					System.out.println("Invalid Acc No ** Must contain numbers");
-					break;
-				}
-			}
-			if(accept==1)
-			{
-				if(accountNumber.length()!=11)
-				{
-					System.out.println("Invalid Acc No ** Length must be 11");
-					accept=0;
-				}
-			}
+			accept = iv.validAccountNumber(accountNumber,accept);
 		}while(accept==0);
+		
+		
 		System.out.println("Account Successfully Created");
 		Account acc = new Account(name,accountNumber);
 		acc.display();
